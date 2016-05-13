c05a907beacdef54f3c6813b98aa62371f8fe91f
Jamie W <whitacre@berkeley.edu>
2016-05-12 22:09:58 -0700

    completed assignment Ch.6 Ex.2

---
+++ conditionals.py
@@ -0,0 +1,95 @@
+### Conditionals Exercises 2
+
+#Q1
+# name1 = "jamie"
+# name2 = "jamie"
+# if name1 > name2:
+# 	print "My name is greater!"
+# elif name1 < name2:
+#     print "Her name is greater!"
+# else: 
+# 	print "Our names are equal!"
+
+#Q2
+# date = 20
+# if date >= 15:
+# 	print "Oh, we're halfway there!"
+# else:
+# 	print "The month is still young."
+
+#Q3
+# today = "Monday"
+# if today == "Monday":
+# 	print "Yay class day!"
+# elif today == "Tuesday":
+# 	print "Sigh, it's only Tuesday."
+# elif today == "Wednesday":
+# 	print "Humpday!"
+# elif today == "Thursday":
+# 	print "#tbt"
+# elif today == "Friday":
+# 	print "TGIF!"
+# else:
+# 	print "Yeah, it's the weekend!"
+
+#Part2
+age = 20
+
+'''Q1 & Q2'''
+# if age >= 18:
+# 	print "Yay! I can vote!"
+# else: 
+# 	print "Aww, I cannot vote."
+
+'''Q3'''
+# if age >= 18 and age >=21:
+# 	print "I can vote and go to a bar!"
+
+'''Q4'''
+# if age >= 21:
+# 	print "I can go to a bar"
+# elif age >= 18 and age <= 21:
+# 	print "I can vote but I cannot go to a bar."
+# else:
+# 	print "I cannot vote or go to a bar."
+
+'''Q5'''
+# if 8 % 2 == 0:
+# 	print "The number 8 is even"
+# else:
+# 	print "The number 8 is odd"
+
+'''Q6'''
+# if 8 % 2 != 0:
+# 	print "The number 8 is odd"
+# else:
+# 	print "The number 8 is even."
+
+'''Q7'''
+# if (8 % 2 == 0) and (9 % 2 == 0):
+# 	print "Both numbers are even."
+# else:
+# 	print "Both numbers are not even."
+
+'''Q8'''
+# if (8 % 2 == 0) and (9 % 2 == 0):
+# 	print "Both numbers are even."
+# elif (8 % 2 == 0) and (9 % 2 != 0):
+# 	print "8 is even and 9 is odd."
+# elif (8 % 2 != 0) and (9 % 2 == 0):
+# 	print "8 is odd and 9 is even."
+# else:
+# 	print "Both numbers are odd."
+
+'''Q9'''
+fav_color = "green"
+# if fav_color == "blue" or fav_color == "Blue":
+# 	print "My favorite color is blue!"
+# else:
+# 	print "My favorite color is not blue."
+
+'''Q10'''
+# if fav_color == "blue" or fav_color == "blue" or fav_color == "blue":
+# 	print "My favorite color is a primary color."
+# else: 
+# 	print "My favorite color is a secondary color."
