5c8ea5f037de9a9f06699b3e15fb63e4a0f93a70
Jamie W <whitacre@berkeley.edu>
2016-05-16 21:38:50 -0700

    Completed Ch7 Ex1 - Functions

---
+++ Class7_Ex1_Functions.py
@@ -0,0 +1,20 @@
+# def hello_print():
+#     print "hello"
+
+# hello_print()
+
+# def hello_return():
+# 	print "hello"
+
+# hello = hello_return()
+
+# print hello
+
+partner_name = "Ray"
+def hello_name(name):
+	print "hello",name
+
+#hello_name("Jamie")
+# hello_name(partner_name)
+print hello_name
+
