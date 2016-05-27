d73f0632ce069e564029802d022c1687a64cc88a
JamieW <whitacre@berkeley.edu>
2016-05-26 21:11:54 -0700

    Ch. 14.1 Code Workshop Classes and APIs

---
+++ contacts_manager_jw.py
@@ -0,0 +1,12 @@
+import contactlist_jw *
+
+contact_list = []
+
+def add(first_name, last_name, mobile_phone="", email="")
+	contact_list.append()
+
+def remove()
+
+def edit()
+
+def display_sorted()
