711e5cf0b06e936993f35b94a0240fc325512cca
Jamie W <whitacre@berkeley.edu>
2016-05-30 20:27:34 -0700

    updated methods, functions

--- contactlist_jw.py
+++ contactlist_jw.py
@@ -1,9 +1,19 @@
-class Contact():
-	def __init__(self, first_name, last_name, mobile_phone="", email=""):
+class Contact(object):
+	def __init__(self, first_name, last_name, 
+				mobile_phone="", email=""):
 		self.first_name = first_name
 		self.last_name = last_name
 		self.mobile_phone = mobile_phone
 		self.email = email
 
 	def send_email(self, message):
-		print "To: %s - %s" % (self, email, message)
+		print "To: %s - %s" % (self.email, message)
+
+def save_contact(contact):
+	contacts = []
+	contacts.append(contact)
+	return contacts
+
+def write_to_text(contacts_list):
+	for obj in contacts_list:
+		print obj.__dict__
