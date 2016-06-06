4762360928be4653116209d159ce66621240f1c3
JamieW <whitacre@berkeley.edu>
2016-06-05 22:38:55 -0700

    Added variables for return values

--- BillCalculator.py
+++ BillCalculator.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 bill_amount = float(raw_input("What is the bill amount?> "))
 tip_percentage = float(raw_input("What is the tip percentage?> "))
 number_people = int(raw_input("How many people in your party?> "))
@@ -11,9 +12,9 @@ def total_bill(tip_amount, bill_amount):
 	return total_bill
 
 def split_bill(total_bill, number_people):
-	price_per_person = total_bill/number_people
+	price_per_person = total_bill / number_people
 	return price_per_person
 
-print calculate_tip(bill_amount, tip_percentage)
-print total_bill(tip_amount, bill_amount)
-print split_bill(total_bill, number_people)
+tip_amount = calculate_tip(bill_amount, tip_percentage)
+final_bill = total_bill(tip_amount, bill_amount)
+print split_bill(final_bill, number_people)
